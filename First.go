941440c76100e7b739320a8789e574902bb8f103
hm-shahadat <shahadat.22.48095.2.aiub.edu@gmail.com>
2026-07-09 16:50:52 +0600

    This is my first code and there have GO HOME to GO OUTPUT FUNCTION and from now I creat seperate files for each topic of go code

---
+++ First.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+)
+
+//-----go constrain variable------
+/*const PI = 40
+var P2 int
+var myName = "shahadat"
+const (
+	myName2        = "shakib"
+	idNumber       = 20
+	roadNumber int = 3440
+)
+*/
+
+var i string = "hello world"
+var j int = 20
+
+func main() {
+
+	//--------go home to constants----------
+	/*fmt.Println("please input how many time you want to say hello world")
+	fmt.Scan(&P2)
+	res := PI + P2
+	fmt.Println(myName, "and", myName2, "there id ", idNumber, "and there road number ", roadNumber, "they say hello word ", res, "times")
+	*/
+
+	//---------go output function and formatting verbs-------
+	/*var i, j string = "hello", "world"
+	fmt.Print(i, "\n", j, "\n")
+	*/
+
+	// var i,j = 10,20
+	// fmt.Print(i,j)
+
+	// var i,j = "hello","wrold"
+	// fmt.Println(i,j)
+
+	//----go output print function -------
+	// fmt.Printf("the value is: %v and the type is: %T\n", i,i)
+	// fmt.Printf("the value is: %v and the type is: %T\n", i,j)
+
+	// fmt.Printf("the vaule is: %v%% and the type is: %T\n", j,j)
+	// fmt.Printf("the vaule is: %#v\nand the type is: %T\n", j,j)
+	
+	// var k = true
+	// fmt.Printf("%t", k)
+
+	
+
+
+}
