5eb88d3cb6a59084cd8da6d67a8ea9946e890785
hm-shahadat <shahadat.22.48095.2.aiub.edu@gmail.com>
2026-07-09 18:41:39 +0600

    Go DATA type

--- go-datatype.go
+++ go-datatype.go
@@ -1,8 +1,23 @@
 package main
 import("fmt")
 
-var a bool = false
+var a bool = true
+var b = false
+var c int = 12
+var d string = "hello"
+var e float64 = 3.15555 
+var f int = -45 
+var g uint = 66
+var h float32 = 3.4e+38
+var i string = "hello"
 
 func main() {
-	fmt.Println(a)
+
+	fmt.Printf("value: %v, type: %T", h, h)
+
+
+
+
 }
+
+
