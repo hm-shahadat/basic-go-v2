307cce2e9035292b66348257f6c87efd6f637cb7
hm-shahadat <shahadat.22.48095.2.aiub.edu@gmail.com>
2026-07-16 19:27:18 +0600

    all about loop

--- loop/basic-loop.go
+++ loop/basic-loop.go
@@ -1,26 +1,72 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 
-	// for loop
-	for i := 1; i <= 10; i = i + 2 {
+	// // for loop
+	// for i := 1; i <= 10; i = i + 2 {
 
-		fmt.Printf("shahadat\n")
+	// 	fmt.Printf("shahadat\n")
 
-	}
+	// }
 
-	//while loop
+	// //while loop
 
-	y := 10
-	for y >= 1 {
+	// y := 10
+	// for y >= 1 {
 
-		fmt.Printf("y= %v\n", y)
+	// 	fmt.Printf("y= %v\n", y)
 
-		y = y - 1
-	}
+	// 	y = y - 1
+	// }
+
+	// //use continue and brake
+
+	// for z := 1; z < 10; z++ {
+
+	// 	if z == 3 {
+
+	// 		continue
+	// 	}
+
+	// 	fmt.Println(z)
+	// }
+
+	// for z := 1; z < 10; z++ {
+
+	// 	if z%2 == 0 {
+
+	// 		break
+	// 	}
+
+	// 	fmt.Println(z)
+	// }
+
+	//nested loop
+
+	// comment := [3]string{"hello", "running", "by"}
+	// country := [5]string{"bd", "italy", "spain", "germany", "malta"}
+
+	// for a := 0; a < len(comment); a++ {
+
+	// 	for b := 0;   b < len(country); b++ {
+
+	// 		fmt.Println(comment[b], country[a])
+
+	// 	}
+
+	// var n int
+	// fmt.Println("please enter a Nth number:")
+	// fmt.Scan(&n)
+	// sum := 0
+
+	// // for i := 1; i <= a; i++ {
+	// // 	sum = sum + i
+	// // }
+
+	// sum = n * (n + 1) / 2
+
+	// fmt.Println(sum)
+
+	// }
 
 }
