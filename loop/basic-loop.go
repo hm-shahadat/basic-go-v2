93030d2c949ca80b78073903c49066485ed50da9
hm-shahadat <shahadat.22.48095.2.aiub.edu@gmail.com>
2026-07-16 12:57:12 +0600

    basic for and while loop

---
+++ loop/basic-loop.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// for loop
+	for i := 1; i <= 10; i = i + 2 {
+
+		fmt.Printf("shahadat\n")
+
+	}
+
+	//while loop
+
+	y := 10
+	for y >= 1 {
+
+		fmt.Printf("y= %v\n", y)
+
+		y = y - 1
+	}
+
+}
