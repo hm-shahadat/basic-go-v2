7e7290395d4a98f66b30e9e2630a4454fce1eaeb
hm-shahadat <shahadat.22.48095.2.aiub.edu@gmail.com>
2026-08-23 12:08:53 +0600

    array2

--- Array2.go
+++ Array2.go
@@ -4,14 +4,28 @@ import "fmt"
 
 func main() {
 
-	var arr [5]string
+	var arr [3]string
 
-	for i := 0; i < 5; i++ {
-		fmt.Print("Please enter 5 student name: ")
+	// Print Array using loop
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Print("Please enter 5 student name: ")
+	// 	fmt.Scan(&arr[i])
+
+	// }
+
+	// fmt.Println(arr)
+
+	// user input index number and then print that index value
+	// var choice int
+	// fmt.Print("please enter e number: ")
+	// fmt.Scan(&choice)
+
+	// fmt.Println(arr[choice])
+
+	for i := 0; i < 3; i++ {
+
+		fmt.Print("please enter your full name in 3 step:")
 		fmt.Scan(&arr[i])
-
 	}
-
 	fmt.Println(arr)
-
 }
