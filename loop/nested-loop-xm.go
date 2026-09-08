d219ef807851ce5bcfb3c674884f2315c28e7ce4
hm-shahadat <shahadat.22.48095.2.aiub.edu@gmail.com>
2026-09-08 15:26:21 +0600

    loop

--- loop/nested-loop-xm.go
+++ loop/nested-loop-xm.go
@@ -8,9 +8,11 @@ func main() {
 	fmt.Println("please enter n-th integer number:")
 	fmt.Scan(&a)
 
-	for i := a; i >= 1; i-- { i=5, 
+	for i := a; i >= 1; i-- {
+		i = 5
 
-		for j := 1; j <= i; j++ { j = 1 
+		for j := 1; j <= i; j++ {
+			j = 1
 			fmt.Print("*")
 		}
 
