a05bf2e81ef571cc02a7afe80d5102c8c5a16afe
hm-shahadat <shahadat.22.48095.2.aiub.edu@gmail.com>
2026-09-10 13:08:37 +0600

    variadic_function

--- function/variadic_function.go
+++ function/variadic_function.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// we can also use 'interface{}' for use any types of value
 func variD(nums ...int) int {
 
 	total := 0
@@ -18,4 +19,10 @@ func variD(nums ...int) int {
 func main() {
 	result := variD(3, 4, 5, 6)
 	fmt.Println(result)
+
+	// 	we can also use slice instead of 'result'
+	slic := []int{3, 4, 5, 6, 7}
+	result2 := variD(slic...)
+	fmt.Println(result2)
+
 }
